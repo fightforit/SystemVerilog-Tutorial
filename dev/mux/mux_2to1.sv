e36a990c220c5e842f4e47507e12166bd33cfab8
Cheng-Hsun Lu <lchsun@z02.eecs.umich.edu>
2022-10-09 14:19:33 -0400

    create a mux module

--- dev/mux/mux_2to1.sv
+++ dev/mux/mux_2to1.sv
@@ -1,4 +1,27 @@
-module mux_2to1 ();
+module mux_2to1 #(
+    parameter int BIT_WIDTH = 8
+) (
+    input  logic                 sel_i,
+    input  logic [BIT_WIDTH-1:0] a_i,
+    input  logic [BIT_WIDTH-1:0] b_i,
+    output logic [BIT_WIDTH-1:0] y_o
+);
 
+  // Continuous assignment
+  assign y = sel ? b : a;
+
+  // Procedural 1
+  always_comb begin
+    y = sel ? b : a;
+  end
+
+  // Procedural 2
+  always_comb begin
+    if (sel) begin
+      y = b;
+    end else begin
+      y = a;
+    end
+  end
 
 endmodule : mux_2to1
