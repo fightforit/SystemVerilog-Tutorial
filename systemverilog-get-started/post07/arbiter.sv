24fd349b9b90e713c40e4a7acd0318d87afca751
Shou-Li Hsu <hank850503@gmail.com>
2023-01-24 00:38:17 +0800

    Update arbiter design

--- systemverilog-get-started/post07/arbiter.sv
+++ systemverilog-get-started/post07/arbiter.sv
@@ -5,11 +5,17 @@ module Arbiter #(
     output logic [N-1:0] oGrant
 );
 
+  logic granted;
+
   // request at smaller index has higher priority
   always_comb begin
-    oGrant[0] = iRequest[0];
-    for (int i = 1; i < N; i++) begin
-      oGrant[i] = iRequest[i] & ~|(oGrant << (N - i));
+    granted = 1'b0;
+    oGrant  = '0;
+    for (int i = 0; i < NumRequest; i++) begin
+      if (iRequest[i] & ~granted) begin
+        oGrant[i] = 1'b1;
+        granted   = 1'b1;
+      end
     end
   end
 
@@ -22,7 +28,7 @@ module Arbiter #(
   */
 
   // break method
-  /* 
+  /*
   always_comb begin
     oGrant = '0;
     for (int i = 0; i < N; i++) begin
